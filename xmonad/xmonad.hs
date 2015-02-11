ebf6771797d001cf21a5636f46495a5110e91daa
Irish <boylemic@gmail.com>
2015-02-11 13:48:41 -0600

    Update xmonad.hs

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,99 +1,129 @@
-import XMonad hiding ( (|||), Tall )
+-- Imports.
+import XMonad
 import XMonad.Hooks.DynamicLog
-import XMonad.Layout.NoBorders
-import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.Place
-import XMonad.Hooks.ManageHelpers
-import XMonad.Util.CustomKeys
-import XMonad.Util.EZConfig
-import XMonad.Layout.Magnifier as Mag
-import XMonad.Layout.BoringWindows
-import XMonad.Layout.MouseResizableTile
-import XMonad.Layout.SimpleFloat
-import XMonad.Layout.DragPane
-import XMonad.Layout.TwoPane
-import XMonad.Layout.LayoutCombinators
-import XMonad.Layout.Tabbed
+import XMonad.Hooks.ManageDocks    -- dock/tray mgmt
 import Data.Monoid
-import XMonad.Layout.Named
-import XMonad.Actions.CycleSelectedLayouts as Cycle
-import XMonad.Actions.RotSlaves
-import XMonad.Layout.WindowNavigation
 import qualified XMonad.StackSet as W
-import qualified Data.Map as M
+import qualified Data.Map        as M
 import System.Exit
+-- The main function.
+main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
 
-main = xmonad $ defaultConfig
-                { terminal                                                = "urxvt"
-                , modMask                                                               = mod1Mask --rebind Mod to Windows Key
-                , manageHook                                            = myManageHook
-                , layoutHook                                            = myLayoutHook
-                , focusedBorderColor    = "#ee9a00"
-                , normalBorderColor                     = "#000000"
-                , startupHook                                           = startup
-                , workspaces = ["1","2","3","4","5","6","7","8","9"]
-                }
+-- Command to launch the bar.
+myBar = "xmobar"
 
-                `additionalKeys`
-                [ ((mod1Mask .|. shiftMask, xK_Return), spawn "urxvt")
--- launch dmenu
-                    , ((mod1Mask, xK_d ), spawn "exe=`dmenu_path | dmenu` && eval \"exec $exe\"")
--- launch gmrun
-                    , ((mod1Mask .|. shiftMask, xK_p ), spawn "gmrun")
--- close focused window
-                    , ((mod1Mask .|. shiftMask, xK_c ), kill)
-                    
-                    , ((mod1Mask, xK_n ), refresh)
--- Move focus to the next window
-                    , ((mod1Mask, xK_Tab ), windows W.focusDown)
--- Move focus to the next window
-                    , ((mod1Mask, xK_j ), windows W.focusDown)
--- Move focus to the previous window
-                    , ((mod1Mask, xK_k ), windows W.focusUp )
--- Move focus to the master window
-                    , ((mod1Mask, xK_m ), windows W.focusMaster )
--- Swap the focused window and the master window
-                    , ((mod1Mask, xK_Return), windows W.swapMaster)
--- Swap the focused window with the next window
-                    , ((mod1Mask .|. shiftMask, xK_j ), windows W.swapDown )
--- Swap the focused window with the previous window
-                    , ((mod1Mask .|. shiftMask, xK_k ), windows W.swapUp )
--- Shrink the master area
-                    , ((mod1Mask, xK_h ), sendMessage Shrink)
--- Expand the master area
-                    , ((mod1Mask, xK_l ), sendMessage Expand)
--- Push window back into tiling
-                    , ((mod1Mask, xK_t ), withFocused $ windows . W.sink)
--- Increment the number of windows in the master area
-                    , ((mod1Mask , xK_comma ), sendMessage (IncMasterN 1))
--- Deincrement the number of windows in the master area
-                    , ((mod1Mask , xK_period), sendMessage (IncMasterN (-1)))
--- Toggle the status bar gap
--- Use this binding with avoidStruts from Hooks.ManageDocks.
--- See also the statusBar function from Hooks.DynamicLog.
--- 
--- , ((modm , xK_b ), sendMessage ToggleStruts)
--- Quit xmonad
-                    , ((mod1Mask .|. shiftMask, xK_q ), io (exitWith ExitSuccess))
--- Restart xmonad
-                    , ((mod1Mask , xK_q ), spawn "xmonad --recompile; xmonad --restart")
-                ]
+-- Custom PP, configure it as you like. It determines what is being written to the bar.
+myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "" "" }
 
-myLayoutHook = windowNavigation . avoidStruts . smartBorders . Mag.maximizeVertical $ (named "default" mouseResizableTile ||| named "two pane" (TwoPane (3/100) (1/2) ) |||  named "full" Full ||| gimpLayout ||| pdfLayout)
-        where
-        gimpLayout = named "gimp layout" (simpleTabbed ****||* simpleTabbed)
-        pdfLayout = named "pdf layout" (simpleTabbed *||* mouseResizableTile)
+-- Key binding to toggle the gap for the bar.
+toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 
-myManageHook = composeAll $
-        [ resource =? name --> doIgnore | name <- ignore ]
-        ++[ title =? name --> doCenterFloat | name <- floaters ]
-        ++[ resource =? name --> doCenterFloat | name <- floaters ]
-        ++[ manageDocks <+> manageHook defaultConfig
-                ,(isFullscreen --> doFullFloat) --full float fullscreen flash
-                ]
-        where
-                floaters = ["xmos2print","xcalc", "galculator", "gcalctool"]
-                ignore = ["stalonetray"]
-                                         
-startup :: X ()
-startup = return ()
+-- Main configuration, override the defaults to your liking.
+myConfig = defaultConfig { modMask= mod1Mask
+	         , terminal = "urxvt"
+	         , workspaces = myWorkspaces
+	         , keys = myKeys
+                         , focusedBorderColor = "#2E9AFE"
+                         , normalBorderColor = "#000000"
+                         , manageHook = myManageHook
+	         }
+
+myWorkspaces    = ["1:www","2:ter","3:mail","4:ranger","5:steam","6","7","8","9"]
+myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
+ 
+    -- launch a terminal
+    [ ((mod1Mask .|. shiftMask, xK_Return), spawn "urxvt")
+ 
+    -- launch dmenu
+    , ((modMask,               xK_d     ), spawn "exe=`dmenu_path | dmenu` && eval \"exec $exe\"")
+ 
+    -- launch gmrun
+    , ((modMask .|. shiftMask, xK_p     ), spawn "gmrun")
+ 
+    -- close focused window
+    , ((modMask .|. shiftMask, xK_c     ), kill)
+ 
+     -- Rotate through the available layout algorithms
+    , ((modMask,               xK_space ), sendMessage NextLayout)
+ 
+    --  Reset the layouts on the current workspace to default
+    , ((modMask .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
+ 
+    -- Resize viewed windows to the correct size
+    , ((modMask,               xK_n     ), refresh)
+ 
+    -- Move focus to the next window
+    , ((modMask,               xK_Tab   ), windows W.focusDown)
+ 
+    -- Move focus to the next window
+    , ((modMask,               xK_j     ), windows W.focusDown)
+ 
+    -- Move focus to the previous window
+    , ((modMask,               xK_k     ), windows W.focusUp  )
+ 
+    -- Move focus to the master window
+    , ((modMask,               xK_m     ), windows W.focusMaster  )
+ 
+    -- Swap the focused window and the master window
+    , ((modMask,               xK_Return), windows W.swapMaster)
+ 
+    -- Swap the focused window with the next window
+    , ((modMask .|. shiftMask, xK_j     ), windows W.swapDown  )
+ 
+    -- Swap the focused window with the previous window
+    , ((modMask .|. shiftMask, xK_k     ), windows W.swapUp    )
+ 
+    -- Shrink the master area
+    , ((modMask,               xK_h     ), sendMessage Shrink)
+ 
+    -- Expand the master area
+    , ((modMask,               xK_l     ), sendMessage Expand)
+ 
+    -- Push window back into tiling
+    , ((modMask,               xK_t     ), withFocused $ windows . W.sink)
+ 
+    -- Increment the number of windows in the master area
+    , ((modMask              , xK_comma ), sendMessage (IncMasterN 1))
+ 
+    -- Deincrement the number of windows in the master area
+    , ((modMask              , xK_period), sendMessage (IncMasterN (-1)))
+ 
+    -- Toggle the status bar gap
+    -- Use this binding with avoidStruts from Hooks.ManageDocks.
+    -- See also the statusBar function from Hooks.DynamicLog.
+    --
+    -- , ((modm              , xK_b     ), sendMessage ToggleStruts)
+ 
+    -- Quit xmonad
+    , ((modMask .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
+ 
+    -- Restart xmonad
+    , ((modMask              , xK_q     ), spawn "xmonad --recompile; xmonad --restart")
+    ]
+    ++
+ 
+    --
+    -- mod-[1..9], Switch to workspace N
+    --
+    -- mod-[1..9], Switch to workspace N
+    -- mod-shift-[1..9], Move client to workspace N
+    --
+    [((m .|. modMask, k), windows $ f i)
+        | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
+        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+    ++
+ 
+    --
+    -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
+    -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
+    --
+    [((m .|. modMask, key), screenWorkspace sc >>= flip whenJust (windows . f))
+        | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
+        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+
+myManageHook = composeAll
+    [ className =? "stalonetray"    --> doIgnore
+    
+    ]
+
+
